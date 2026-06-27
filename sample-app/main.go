9e99f60a70c16c95511f69701c5aff893080b07b
jiyongham <o1067426676@gmail.com>
2026-06-27 13:28:47 +0900

    cicd practice

---
+++ sample-app/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+var version = "dev"
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello from sample-app! version: %s\n", version)
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+func main() {
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/health", healthHandler)
+	http.ListenAndServe(":8080", nil)
+}
